ce5538e9664392a90bc1ef008607531d47963933
Scott Chamberlain <myrmecocystus@gmail.com>
2016-04-05 14:52:03 -0700

    allow user to pass in a key to bhl functions

--- R/sp_bhl_meta.R
+++ R/sp_bhl_meta.R
@@ -2,22 +2,23 @@
 #'
 #' @export
 #' @param x input
+#' @param key api key, optional
 #' @examples \dontrun{
 #' geom <- 'POLYGON((-124.07 41.48,-119.99 41.48,-119.99 35.57,-124.07 35.57,-124.07 41.48))'
 #' res <- sp_occ(geometry = geom)
 #' res %>% sp_list()
 #' x <- res %>% sp_list() %>% sp_bhl_meta()
 #' }
-sp_bhl_meta <- function(x) {
+sp_bhl_meta <- function(x, key = NULL) {
   out <- list()
   for (i in seq_along(x)) {
     # search to get namebankid
-    z <- tryCatch(bhl_namesearch(name = x[[i]]), error = function(e) e)
+    z <- tryCatch(bhl_namesearch(name = x[[i]], key = key), error = function(e) e)
     if (z$data[1, 'NameBankID'] == "" || inherits(z, "error")) {
       out[[i]] <- NULL
     } else {
       # get BHL pages with that namebankid
-      yy <- bhl_namegetdetail(namebankid = z$data[1, 'NameBankID'])
+      yy <- bhl_namegetdetail(namebankid = z$data[1, 'NameBankID'], key = key)
       # get page details for each result
       out[[i]] <- lapply(yy$data$Titles.Items, function(z) {
         pgs <- do.call("rbind.data.frame", z$Pages)
